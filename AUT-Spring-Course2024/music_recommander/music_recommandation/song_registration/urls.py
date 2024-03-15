65b43758b425870af693810952f1811836b6b64e
Abtinz <abtinzandi@gmail.com>
2024-03-15 19:03:53 +0330

    shezam api and others urls are added to main app

--- AUT-Spring-Course2024/music_recommander/music_recommandation/song_registration/urls.py
+++ AUT-Spring-Course2024/music_recommander/music_recommandation/song_registration/urls.py
@@ -3,5 +3,7 @@ from django.urls import path , include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("registration/", include("song_registration.urls"))
+    path("registration/", include("song_registration.urls")),
+    path("recommender/", include("recommender_system.urls")),
+    path("shezam/", include("shezam_service.urls"))
 ]
