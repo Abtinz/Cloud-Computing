228ad6fbf2f7dab5011eb14db393022c52089cf0
Abtinz <abtinzandi@gmail.com>
2024-03-15 22:24:09 +0330

    new model for saving user request

--- AUT-Spring-Course2024/music_recommander/music_recommandation/song_registration/models.py
+++ AUT-Spring-Course2024/music_recommander/music_recommandation/song_registration/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# user song requests model
+class Request(models.Model):
+    
+    #id will build and  increment as a primary key
+    id=models.AutoField(primary_key=True)
+
+    #this is our users email field for mail service section
+    email = models.EmailField()
+
+    #this status class will save status of request
+    class Status(models.TextChoices):
+            done = "done"  
+            failure = "failure"
+            pending = "pending"
+            ready = "ready"
+
+    status=models.CharField(max_length=50,choices=Status.choices,default=Status.pending)
+
+    song_id = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.description
