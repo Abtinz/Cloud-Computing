1d02d74f59081f3c0a467e2c95ec66f2c2df4f4c
Abtinz <abtinzandi@gmail.com>
2024-03-15 19:07:15 +0330

    urls are tested

--- AUT-Spring-Course2024/music_recommander/music_recommandation/music_recommandation/urls.py
+++ AUT-Spring-Course2024/music_recommander/music_recommandation/music_recommandation/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("registration/", include("song_registration.urls")),
+    path("recommender/", include("recommender_system.urls")),
+    path("shezam/", include("shezam_service.urls"))
 ]
--- AUT-Spring-Course2024/music_recommander/music_recommandation/song_registration/urls.py
+++ AUT-Spring-Course2024/music_recommander/music_recommandation/song_registration/urls.py
@@ -3,7 +3,4 @@ from django.urls import path , include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("registration/", include("song_registration.urls")),
-    path("recommender/", include("recommender_system.urls")),
-    path("shezam/", include("shezam_service.urls"))
 ]
