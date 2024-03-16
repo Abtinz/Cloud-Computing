5a55db7af3593ee14ea9a53d37b4911876023cb8
Abtinz <abtinzandi@gmail.com>
2024-03-16 16:03:51 +0330

    this data class will save users requests for songs in database

--- AUT-Spring-Course2024/music_recommendation/song_registration/models.py
+++ AUT-Spring-Course2024/music_recommendation/song_registration/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
-class Request(models.Model):
+"""
+    this data class will save users requests for songs in database
+    id is primary key for each request
+    user email will be used for shezam api results, if api find the related songs, this email will receive a message of them
+    song_id is an id for song ,will find from shezam api
+"""
+class SongRequests(models.Model):
+
+    id=models.AutoField(primary_key=True)
     email = models.EmailField()
-    status = models.CharField(max_length=20, choices=['pending', 'failure', 'ready','done'], default='pending')
+    status = models.CharField(choices=['pending', 'failure', 'ready','done'], default='pending')
     song_id = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.song_id + "//" + self.email
+
+    
