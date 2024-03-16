46564b91c1959b39a6e60b9952c3be7d717a3150
Abtinz <abtinzandi@gmail.com>
2024-03-16 16:11:48 +0330

    fundamental first service request structures

--- AUT-Spring-Course2024/music_recommendation/song_registration/views.py
+++ AUT-Spring-Course2024/music_recommendation/song_registration/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from .models import SongRequests
+from .serializers import SongRequestsSerializer,AddSongRequestsSerializer
+from music_recommendation.S3_helper import upload_to_server ,create_song_url
+import requests
 
-# Create your views here.
+
+
+class MusicUploadView(generics.CreateAPIView):
+    
+    def post(self,request):
+        
+        return Response({"error": "email or file is empty"}, status=400)
