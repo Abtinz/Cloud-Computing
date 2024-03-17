d80159416abf48af5d6e50b8f80cbc57c5afc21d
Abtinz <abtinzandi@gmail.com>
2024-03-18 02:46:45 +0330

    mailgun service

--- AUT-Spring-Course2024/music_recommendation/song_registration/views.py
+++ AUT-Spring-Course2024/music_recommendation/song_registration/views.py
@@ -7,6 +7,7 @@ from .serializers import SongRequestsSerializer,AddSongRequestsSerializer
 from music_recommendation.S3_helper import upload_to_server ,create_song_url
 import requests
 from rest_framework import generics , status
+from music_recommendation.mailgun import mailgun_service
 
 class MusicRequestView(generics.CreateAPIView):
     serializer_class=AddSongRequestsSerializer
@@ -51,6 +52,7 @@ class MusicRequestView(generics.CreateAPIView):
 class RegisterApiMainView(APIView):
     
     def get(self,request):
+        print(mailgun_service(email = "abtinzandi@gmail.com" , message= "test hello"))
         return Response({"warning": "song register system url: https://music-recommender-cloud.liara.run/register/song/"}, status=300)
     
 class GetSongRequestsView(generics.RetrieveAPIView):
