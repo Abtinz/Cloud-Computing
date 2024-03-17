e2594f3f1ace3cc5a9855c6ac1d2f9016bb0d4b4
Abtinz <abtinzandi@gmail.com>
2024-03-17 18:23:03 +0330

    view for test

--- AUT-Spring-Course2024/music_recommendation/song_registration/views.py
+++ AUT-Spring-Course2024/music_recommendation/song_registration/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
+from rest_framework.views import APIView
 from .models import SongRequests
 from .serializers import SongRequestsSerializer,AddSongRequestsSerializer
 from music_recommendation.S3_helper import upload_to_server ,create_song_url
 import requests
 
 
-
-class MusicUploadView(generics.CreateAPIView):
+class MusicRequestView(APIView):
     
     def post(self,request):
         
-        return Response({"error": "email or file is empty"}, status=400)
+        return Response({"": ""}, status=200)
