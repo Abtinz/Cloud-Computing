8ee1558faed3336395f1ac10955b83aebb4006ca
Abtinz <abtinzandi@gmail.com>
2024-03-17 22:03:57 +0330

    view music and email recorder

--- AUT-Spring-Course2024/music_recommendation/song_registration/views.py
+++ AUT-Spring-Course2024/music_recommendation/song_registration/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import status
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from .models import SongRequests
 from .serializers import SongRequestsSerializer,AddSongRequestsSerializer
 from music_recommendation.S3_helper import upload_to_server ,create_song_url
 import requests
+from rest_framework import generics
 
-
-class MusicRequestView(APIView):
-    
+class MusicRequestView(generics.CreateAPIView):
+    serializer_class=AddSongRequestsSerializer
+    queryset=SongRequests.objects.all()
     def post(self,request):
-        
+         
         return Response({"": ""}, status=200)
 
 #this api vie will help user to find specific url of song request api
