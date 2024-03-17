2c0bf6643f7ab5bfb29ebfb0c2cff58b6e2022e7
Abtinz <abtinzandi@gmail.com>
2024-03-17 22:10:47 +0330

    song req processing

--- AUT-Spring-Course2024/music_recommendation/song_registration/views.py
+++ AUT-Spring-Course2024/music_recommendation/song_registration/views.py
@@ -12,7 +12,28 @@ class MusicRequestView(generics.CreateAPIView):
     serializer_class=AddSongRequestsSerializer
     queryset=SongRequests.objects.all()
     def post(self,request):
-         
+
+        #receiving the email of user from json form
+        request_data={
+            "email":request.data["email"]
+        }
+
+        serializer=SongRequestsSerializer(data=request_data)
+        #check if the data is valid
+        if(serializer.is_valid()):
+
+            #now we need to save and upload our data to Dbas and S3 cloud services
+            
+            serializer.save()
+            song_request=serializer.data
+
+            #uploading the request and song to the server
+            request_id =song_request['id']
+            file=request.data['image']
+        else:
+            pass
+
+
         return Response({"": ""}, status=200)
 
 #this api vie will help user to find specific url of song request api
