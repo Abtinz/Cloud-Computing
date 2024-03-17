19a09afe2f886fc3b528fb0f35908c8b57fd11d7
Abtinz <abtinzandi@gmail.com>
2024-03-18 03:19:12 +0330

    song url is added

--- AUT-Spring-Course2024/music_recommendation/song_registration/models.py
+++ AUT-Spring-Course2024/music_recommendation/song_registration/models.py
@@ -12,6 +12,7 @@ class SongRequests(models.Model):
     email = models.EmailField()
     status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('failure', 'Failure'), ('ready', 'Ready'), ('done', 'Done')], default='pending')
     song_id = models.CharField(max_length=255, blank=True, null=True)
+    song_url = models.CharField(max_length=500,blank=True,null=True)
 
     def __str__(self):
         return self.song_id + "//" + self.email
