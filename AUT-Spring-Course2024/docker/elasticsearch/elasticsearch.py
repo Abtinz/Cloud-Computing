e59807102a86d12115889fa978c386e1bb6fff39
Abtinz <abtinzandi@gmail.com>
2024-04-12 15:03:06 +0330

    elasticsearch

---
+++ AUT-Spring-Course2024/docker/elasticsearch/elasticsearch.py
@@ -0,0 +1,42 @@
+import requests
+from elasticsearch import Elasticsearch
+import time
+
+
+ELASTIC_USERNAME = 'elastic'
+ELASTIC_PASSWORD = '12345'
+
+
+def elasticsearch_initializing():
+    url = 'https://elasticsearch:9200/_bulk?pretty'
+    json_file = 'movies.json'
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    with open(json_file, 'rb') as f:
+        data = f.read()
+
+    response = requests.post(url, auth=(ELASTIC_USERNAME, ELASTIC_PASSWORD), headers=headers, data=data, verify=False)
+
+    while response.status_code != 200:
+        print("Error during making index, wait")
+        time.sleep(10)
+        response = requests.post(url, auth=(ELASTIC_USERNAME, ELASTIC_PASSWORD), headers=headers, data=data,verify=False)
+    print("Elasticsearch database initialized successfully!")
+
+
+class ElasticSearchService:
+    def __init__(self):
+        self.db = Elasticsearch("https://elasticsearch:9200", http_auth=(ELASTIC_USERNAME, ELASTIC_PASSWORD), verify_certs=False)
+        if self.db.ping():
+            print(f"connected to elasticsearch service {self.db.ping()}")
+
+    def search(self, term):
+        result = self.db.search(index='_all', body={"query": {"match": {"Series_Title": term}}})
+        output = [hit['_source'] for hit in result['hits']['hits']]
+        if output:
+            return output, True
+        else:
+            return None, False
