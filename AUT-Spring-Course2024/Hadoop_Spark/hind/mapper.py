8fc4aaf776e1c45fc8355665fcf7724c9448e19a
Abtinz <abtinzandi@gmail.com>
2024-05-24 21:04:15 +0330

    map phase

--- AUT-Spring-Course2024/Hadoop_Spark/hind/mapper.py
+++ AUT-Spring-Course2024/Hadoop_Spark/hind/mapper.py
@@ -3,3 +3,13 @@
 It processes each line to extract words and emits each word along with the document ID it appeared in. 
 Each word is emitted only once per document to ensure uniqueness.
 """
+
+import sys
+
+for line in sys.stdin:
+    document_id, text = line.split(',')
+    #here we will find each word of each document
+    words = text.split()
+    
+    for word in words:
+        print(f'{document_id}\t{word}')
