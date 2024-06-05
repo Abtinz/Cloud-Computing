78b853cba75d7895db0d8ee47729962e7f1984f2
Abtinz <abtinzandi@gmail.com>
2024-06-06 00:18:41 +0330

    fire clusters

--- AUT-Spring-Course2024/cassandra/scripts.sh
+++ AUT-Spring-Course2024/cassandra/scripts.sh
@@ -1,1 +1,17 @@
 docker pull cassandra
+
+#for test
+docker run --rm -d --name cassandra-db cassandra:latest
+
+docker ps
+
+docker stop cassandra-db
+
+#lets fire the clusters
+docker compose up -d
+docker ps
+docker exec -it firt-cassandra-service nodetool info
+docker exec -it second-cassandra-service nodetool info
+docker exec -it third-cassandra-service nodetool info
+
+docker dpwn up -d
