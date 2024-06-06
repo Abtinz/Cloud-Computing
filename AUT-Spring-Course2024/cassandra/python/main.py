4eb17c59ed43ed78c1be916222da4adf8c5db01c
Abtinz <abtinzandi@gmail.com>
2024-06-06 14:01:41 +0330

    scripts updated

--- AUT-Spring-Course2024/cassandra/python/main.py
+++ AUT-Spring-Course2024/cassandra/python/main.py
@@ -1,4 +1,4 @@
 from cassandra.cluster import Cluster
 
 cluster = Cluster(['0.0.0.0'], port=9042)
-session = cluster.connect('employee')
+session = cluster.connect('shop')
--- AUT-Spring-Course2024/cassandra/python/scripts.sh
+++ AUT-Spring-Course2024/cassandra/python/scripts.sh
@@ -3,7 +3,7 @@ pip install -r requirements.txt
 docker pull cassandra
 
 #for test
-docker run --rm -d --name cassandra-db  -p 9042:9042 cassandra:latest
+docker run --rm -d --name cassandra-shop  -p 9042:9042 cassandra:latest
 
 docker ps
 
