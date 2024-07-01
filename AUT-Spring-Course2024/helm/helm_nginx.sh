b1365c9416a9bd7863e5fbb4a53d1f29b361f492
Abtinz <abtinzandi@gmail.com>
2024-07-01 10:38:22 +0330

    helm and nginx

---
+++ AUT-Spring-Course2024/helm/helm_nginx.sh
@@ -0,0 +1,8 @@
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+#nginx
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install nginx-ingress ingress-nginx/ingress-nginx
+
+kubectl get service --namespace default nginx-ingress-ingress-nginx-controller --output wide --watch
